53f903745aee7522544aae7b2b7afe84bab06c36
Philip Graf <philip.grf@gmail.com>
2013-12-10 00:18:32 +0100

    linked list

--- datastructure.go
+++
@@ -1,7 +0,0 @@
-package datastructure
-
-import "fmt"
-
-func Test(){
-  fmt.Println("Hello World")
-}
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/philipgraf/datastructure"
-
-func main(){
-  datastructure.Test()
-}
